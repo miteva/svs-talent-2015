6bee395105151598b05243f63b20f89fac35550d
miteva <miteva.katerina92@gmail.com>
2015-04-23 02:59:29 +0200

    Revert "nomvc"
    This reverts commit 6b7e5b75ad8db6f5f4b5f224070cf6ef226732c1.

---
+++ MvcFirst/MvcFirst/MvcFirst/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MvcFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<BikeModel> _bikes = new List<BikeModel>();
+            _bikes.Add(new BikeModel() { Colour = "Red", Model = "CityBike", Prdoucer = "JB", RegNumber = "007", City="Skopje" });
+            _bikes.Add(new BikeModel() { Colour = "Red", Model = "CityBike", Prdoucer = "JB", RegNumber = "007", City = "Skopje" });
+            _bikes.Add(new BikeModel() { Colour = "Red", Model = "CityBike", Prdoucer = "JB", RegNumber = "007", City = "Skopje" });
+            _bikes.Add(new BikeModel() { Colour = "Red", Model = "CityBike", Prdoucer = "JB", RegNumber = "007", City = "Skopje" });
+            return View(_bikes);
+        }
+
+        public ActionResult Profile() {
+
+            return View();
+        }
+    }
+}
