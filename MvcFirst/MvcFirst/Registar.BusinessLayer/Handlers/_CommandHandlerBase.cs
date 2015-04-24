2d1db54f883e54f941fa339af77cf3980fe286a4
miteva <miteva.katerina92@gmail.com>
2015-04-24 16:45:56 +0200

    PL,BL,DL

---
+++ MvcFirst/MvcFirst/Registar.BusinessLayer/Handlers/_CommandHandlerBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registar.BusinessLayer.Contracts
+{
+    /// <summary>
+    /// Abstract class for all command
+    /// </summary>
+    internal abstract class CommandHandlerBase<TRequest,TResult> : IHandler where TRequest : Command where TResult : CommandResult
+    {
+        /// <summary>
+        /// method that execute the command
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        protected abstract TResult ExecuteCommand(TRequest command);
+
+        public CommandResult Execute(Command command)
+        {
+            return ExecuteCommand((TRequest)command);
+        }
+    }
+    internal interface IHandler
+    {
+           CommandResult Execute(Command command);
+    }
+}
